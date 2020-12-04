6213793d1f42f1c5d172947a6480953365b64541
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-04 11:19:15 -0500

    new-day.js

---
+++ new-day.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+const day = args[0];
+
+const setup = `const fs = require('fs');
+
+const input = fs.readFileSync('./raw-input.txt', 'utf8');
+
+const arr = input.split('\\n');
+
+fs.writeFileSync('./input.json', JSON.stringify(arr));`;
+
+const solution = `const arr = require('./input.json');`
+
+fs.mkdirSync(day);
+fs.writeFileSync(`./${day}/setup.js`, setup);
+fs.writeFileSync(`./${day}/solution.js`, solution);
+fs.writeFileSync(`./${day}/raw-input.txt`, '');
+fs.writeFileSync(`./${day}/input.json`, '');
