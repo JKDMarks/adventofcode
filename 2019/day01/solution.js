a110f63b861d4e3e02ea11cace3b541132615aa3
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-04 18:35:22 -0500

    2019/day01

---
+++ 2019/day01/solution.js
@@ -0,0 +1,22 @@
+const arr = require("./input.json");
+
+const calcFuel = (mass) => {
+  const possibleFuel = Math.floor(mass / 3) - 2;
+  return possibleFuel >= 0 ? possibleFuel : 0;
+};
+
+const totalFuel = arr.reduce((acc, cur) => acc + calcFuel(cur), 0);
+
+console.log(totalFuel);
+
+const totalFuel2 = arr.reduce((acc, cur) => {
+  let currFuel = calcFuel(cur);
+  let total = currFuel;
+  while (currFuel > 0) {
+    currFuel = calcFuel(currFuel);
+    total += currFuel;
+  }
+  return acc + total;
+}, 0);
+
+console.log(totalFuel2);
