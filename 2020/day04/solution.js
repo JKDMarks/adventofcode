e3769094f0f41cac873670761f6296f74a54485b
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-04 22:01:09 -0500

    all fancy, all regex

--- 2020/day04/solution.js
+++ 2020/day04/solution.js
@@ -1,26 +1,10 @@
 const arr = require('./input.json');
 
-const validateNumber = (numStr, lb, ub) => {
-	const num = Number(numStr);
-	return Number.isInteger(num) ? lb <= num && num <= ub : false;
-};
-
 const validation = {
-	byr: (byr) => validateNumber(byr, 1920, 2002),
-	iyr: (iyr) => validateNumber(iyr, 2010, 2020),
-	eyr: (eyr) => validateNumber(eyr, 2020, 2030),
-	hgt: (hgt) => {
-		const last2 = hgt.slice(-2);
-		const height = Number(hgt.slice(0, -2));
-
-		if (last2 === 'cm') {
-			return validateNumber(height, 150, 193);
-		} else if (last2 === 'in') {
-			return validateNumber(height, 59, 76);
-		} else {
-			return false;
-		}
-	},
+	byr: (byr) => /^(19[2-9]\d|200[0-2])$/.test(byr),
+	iyr: (iyr) => /^20(1\d|20)$/.test(iyr),
+	eyr: (eyr) => /^20(2\d|30)$/.test(eyr),
+	hgt: (hgt) => /^(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)$/.test(hgt),
 	hcl: (hcl) => /^#[0-9a-f]{6}$/.test(hcl),
 	ecl: (ecl) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl),
 	pid: (pid) => /^\d{9}$/.test(pid),
@@ -40,10 +24,10 @@ const isValid = (pp) => {
 
 const validOnly = arr.filter(isValid);
 
-console.log(validOnly.length);
+console.log(validOnly.length); // 222
 
 const trueValid = validOnly.filter((pp) =>
 	Object.entries(pp).every(([key, val]) => validation[key](val))
 );
 
-console.log(trueValid.length);
+console.log(trueValid.length); // 140
