6b4c89a9a2d8de6c6f9cfdb4e68e85815f40724a
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-04 20:28:06 -0500

    2019/day03

--- 2019/day03/solution.js
+++ 2019/day03/solution.js
@@ -26,8 +26,9 @@ const [wire1, wire2] = wires;
 // };
 
 const traceWires = () => {
-	const visited = {
+	const visited1 = {
 		// '0,0': true,
+		// '0,1': true,
 	};
 	const intersections = [];
 	let currPos = [0, 0];
@@ -47,10 +48,11 @@ const traceWires = () => {
 			}
 
 			const posStr = currPos.join(',');
-			if ('1:' + posStr in visited) {
+			if (wireNum === 1) {
+				visited1[posStr] = true;
+			} else if (wireNum === 2 && posStr in visited1) {
 				intersections.push([...currPos]);
 			}
-			visited[`${wireNum}:${posStr}`] = true;
 		}
 	};
 
@@ -58,14 +60,63 @@ const traceWires = () => {
 	currPos = [0, 0];
 	wire2.forEach((mvmt) => move(mvmt, 2));
 
-	console.log(intersections);
+	console.log('intersections', intersections);
 
 	let closest = Number.MAX_SAFE_INTEGER;
 	intersections.forEach((intArr) => {
 		const dist = Math.abs(intArr[0]) + Math.abs(intArr[1]);
 		closest = Math.min(closest, dist);
 	});
-	console.log(closest);
+	console.log('closest', closest);
+
+	return intersections;
 };
 
-traceWires();
+// traceWires();
+
+const findClosestIntersection = () => {
+	const intersections1 = traceWires().reduce(
+		(acc, cur) => ({ ...acc, [cur.join(',')]: true }),
+		{}
+	);
+	const intersections2 = { ...intersections1 };
+
+	let moveCt = 0;
+	let currPos = [0, 0];
+	const move = (mvmt, wireNum) => {
+		const dir = mvmt[0];
+		const dist = Number(mvmt.slice(1));
+		for (let i = 0; i < dist; i++) {
+			moveCt++;
+			if (dir === 'U') {
+				currPos[1]++;
+			} else if (dir === 'D') {
+				currPos[1]--;
+			} else if (dir === 'R') {
+				currPos[0]++;
+			} else if (dir === 'L') {
+				currPos[0]--;
+			}
+
+			const posStr = currPos.join(',');
+			if (wireNum === 1 && intersections1[posStr] === true) {
+				intersections1[posStr] = moveCt;
+			} else if (wireNum === 2 && intersections2[posStr] === true) {
+				intersections2[posStr] = moveCt;
+			}
+		}
+	};
+
+	wire1.forEach((mvmt) => move(mvmt, 1));
+	currPos = [0, 0];
+	moveCt = 0;
+	wire2.forEach((mvmt) => move(mvmt, 2));
+
+	const intersectionsSum = {};
+	Object.entries(intersections1).forEach(([k, v]) => (intersectionsSum[k] = v));
+	Object.entries(intersections2).forEach(([k, v]) => (intersectionsSum[k] += v));
+
+	return Math.min(...Object.values(intersectionsSum));
+};
+
+console.log(findClosestIntersection());
