6f2e969c6d48927375900090d3a40d65b07568db
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-06 00:22:01 -0500

    idk about this 2019 advent business

--- 2019/day05/solution.py
+++ 2019/day05/solution.py
@@ -2,12 +2,18 @@ import json
 from pdb import set_trace as db
 from pprint import pprint as pp
 
-with open("../day02/input.json") as f:
+with open("input.json") as f:
     input = json.load(f)
 
 
-def run_program(p1, p2):
-    arr = [input[0], p1, p2] + input[3:]
+def run_program(input):
+    output = 0
+    arr = input[:]
+
+    def parse_opcode(unpadded_opcode, a, b, c):
+        opcode = ("0" * (5 - len(unpadded_opcode))) + unpadded_opcode
+        mode3, mode2, mode1, operation = opcode[0], opcode[1], opcode[2], opcode[3:5]
+        return
 
     def handle1(a, b, c):
         arr[c] = arr[a] + arr[b]
@@ -29,12 +35,15 @@ def run_program(p1, p2):
     return arr[0]
 
 
-print(run_program(12, 2))
+for n in input:
+    print(parse_opcode(n))
 
-target = 19690720
+# print(run_program(12, 2))
 
-for x in range(100):
-    for y in range(100):
-        output = run_program(x, y)
-        if output == target:
-            print(x, y, output)  # 67 18 19690720
+# target = 19690720
+
+# for x in range(100):
+#     for y in range(100):
+#         output = run_program(x, y)
+#         if output == target:
+#             print(x, y, output)  # 67 18 19690720
