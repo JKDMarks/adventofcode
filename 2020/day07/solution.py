735f0cb484da40404a4cfd3afb742d132d8489fb
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-07 11:59:37 -0500

    finally got day07

--- 2020/day07/solution.py
+++ 2020/day07/solution.py
@@ -1,35 +1,74 @@
-import json
+import re
 from pdb import set_trace as db
 
-with open("input.json") as f:
-    input = json.load(f)
-
-tgt = "shiny gold"
-
-can_be_contained_by = {}
-for [color, contains] in input.items():
-    for contained_arr in contains:
-        contained_color = contained_arr[1]
-        if contained_color in can_be_contained_by:
-            can_be_contained_by[contained_color].append(color)
-        else:
-            can_be_contained_by[contained_color] = [color]
+with open("raw-input.txt") as f:
+    txt = f.read()
+    input = txt.split("\n")
 
 
-COUNT = 0
-SEEN = {}
+def parse_bag_str(str):
+    str = str.split(" contain ")
+    bag_color = str[0].split(" bags")[0]
+    output = [bag_color, []]
+
+    if str[1] != "no other bags.":
+        contained_bags = str[1].split(", ")
+        for bag in contained_bags:
+            qty = int(bag[0])
+            color_in_bag = re.split(" bag(s)?", bag)[0][2:]
+            output[1].append([qty, color_in_bag])
+
+    return output
 
 
-def count_possiblities(tgt):
-    if tgt in can_be_contained_by:
-        for color in can_be_contained_by[tgt]:
-            if color not in SEEN:
-                SEEN[color] = True
-                # COUNT += 1
-                print(color)
-                count_possiblities(color)
+bag_rules = {}
+for rule in input:
+    [k, v] = parse_bag_str(rule)
+    bag_rules[k] = v
 
 
-count_possiblities(tgt)
-print(COUNT)
-print(len(SEEN.keys()))
+def count_possible_containers(rules, tgt):
+    can_be_contained_by = {}
+    for [containing_color, can_contain_arr] in bag_rules.items():
+        for [qty, contained_color] in can_contain_arr:
+            if contained_color in can_be_contained_by:
+                can_be_contained_by[contained_color].append(containing_color)
+            else:
+                can_be_contained_by[contained_color] = []
+                can_be_contained_by[contained_color].append(containing_color)
+
+    colors_seen = {}
+    to_check = []
+
+    for color in can_be_contained_by[tgt]:
+        colors_seen[color] = True
+        to_check.append(color)
+
+    while len(to_check) > 0:
+        check = to_check.pop(0)
+        if check in can_be_contained_by:
+            for color in can_be_contained_by[check]:
+                if color not in colors_seen:
+                    colors_seen[color] = True
+                    to_check.append(color)
+
+    return len(colors_seen.keys())
+
+
+print(count_possible_containers(bag_rules, "shiny gold"))  # 164
+
+
+def count_contained(rules, tgt, multiplier):
+    count_arr = []
+
+    def run(tgt, multiplier):
+        can_contain_arr = rules[tgt]
+        for [qty, contained_color] in can_contain_arr:
+            count_arr.append(qty * multiplier)
+            run(contained_color, qty * multiplier)
+
+    run(tgt, multiplier)
+    return sum(count_arr)
+
+
+print(count_contained(bag_rules, "shiny gold", 1))  # 7872
