03f403b07622697c7599d2cff3ece270c4b2f8d4
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-09 11:48:32 -0500

    naive brute force solution

---
+++ 2020/day09/solution.py
@@ -0,0 +1,67 @@
+from pdb import set_trace as db
+
+with open("raw-input.txt") as f:
+    input = [int(n) for n in f.read().split("\n")]
+
+test_str = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"""
+test_input = [int(n) for n in test_str.split("\n")]
+
+
+def check_is_valid(chunk, num):
+    for (i, a) in enumerate(chunk):
+        for b in chunk[i + 1 :]:
+            if num == a + b:
+                return True
+    return False
+
+
+def find_first_invalid(input, chunk_size):
+    input = [int(n) for n in input]
+    chunk = input[:chunk_size]
+    i = chunk_size
+    while i < len(input):
+        curr = input[i]
+        if not check_is_valid(chunk, curr):
+            return curr
+        chunk.pop(0)
+        chunk.append(curr)
+        i += 1
+
+
+# print(find_first_invalid(test_input, 5))  # 127
+# print(find_first_invalid(input, 25))  # 167829540
+
+
+def find_weakness(input, tgt_num):
+    for (i, a) in enumerate(input):
+        for (j, b) in enumerate(input[i + 1 :]):
+            weak_arr = input[i : i + j + 1]
+            arr_sum = sum(weak_arr)
+            if arr_sum > tgt_num:
+                break
+            elif arr_sum == tgt_num:
+                print(weak_arr)
+                return min(weak_arr) + max(weak_arr)
+
+
+# print(find_weakness(test_input, 127))  # 62
+# print(find_weakness(input, 167829540))  # 28045630
