043dc4c1765a61b65983ac95d0671d64369f8f56
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-09 12:41:37 -0500

    O(n) instead of O(n^2) or something terrible like that

--- 2020/day09/solution.py
+++ 2020/day09/solution.py
@@ -35,7 +35,6 @@ def check_is_valid(chunk, num):
 
 
 def find_first_invalid(input, chunk_size):
-    input = [int(n) for n in input]
     chunk = input[:chunk_size]
     i = chunk_size
     while i < len(input):
@@ -47,21 +46,39 @@ def find_first_invalid(input, chunk_size):
         i += 1
 
 
-# print(find_first_invalid(test_input, 5))  # 127
-# print(find_first_invalid(input, 25))  # 167829540
+print(find_first_invalid(test_input, 5))  # 127
+print(find_first_invalid(input, 25))  # 167829540
 
+### BRUTE FORCE
+# def find_weakness(input, tgt_num):
+#     for (i, a) in enumerate(input):
+#         for (j, b) in enumerate(input[i + 1 :]):
+#             weak_arr = input[i : i + j + 1]
+#             arr_sum = sum(weak_arr)
+#             if arr_sum > tgt_num:
+#                 break
+#             elif arr_sum == tgt_num:
+#                 print(weak_arr)
+#                 return min(weak_arr) + max(weak_arr)
 
+### ROLLING SUM
 def find_weakness(input, tgt_num):
-    for (i, a) in enumerate(input):
-        for (j, b) in enumerate(input[i + 1 :]):
-            weak_arr = input[i : i + j + 1]
-            arr_sum = sum(weak_arr)
-            if arr_sum > tgt_num:
-                break
-            elif arr_sum == tgt_num:
-                print(weak_arr)
-                return min(weak_arr) + max(weak_arr)
+    start_i = 0
+    end_i = 1
+    arr_sum = sum(input[start_i : end_i])
+    while end_i < len(input):
+        if arr_sum == tgt_num:
+            break
+        elif arr_sum < tgt_num:
+            arr_sum += input[end_i]
+            end_i += 1
+        elif arr_sum > tgt_num:
+            arr_sum -= input[start_i]
+            start_i += 1
+
+    sub_arr = input[start_i : end_i]
+    return min(sub_arr) + max(sub_arr)
 
 
-# print(find_weakness(test_input, 127))  # 62
-# print(find_weakness(input, 167829540))  # 28045630
+print(find_weakness(test_input, 127))  # 62
+print(find_weakness(input, 167829540))  # 28045630
