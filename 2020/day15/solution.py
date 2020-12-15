5a14b16517d2c25a2d61106a70d7da8d6f09d9bd
Jeffrey Marks <JKDMarks@gmail.com>
2020-12-15 12:06:13 -0500

    works but brute force is BRUTE-al 🤣

---
+++ 2020/day15/solution.py
@@ -0,0 +1,46 @@
+import re
+from pdb import set_trace as db
+
+input = [int(n) for n in "15,5,1,4,7,0".split(",")]
+test_input_1 = [int(n) for n in "0,3,6".split(",")]
+test_input_2 = [int(n) for n in "1,3,2".split(",")]
+test_input_3 = [int(n) for n in "3,1,2".split(",")]
+
+
+def nth_num_said(input, tgt_num):
+    turn = 0
+    prev = None
+    last_seen = {}
+
+    for n in input:
+        turn += 1
+        last_seen[n] = [turn]
+        prev = n
+
+    def add_if_exists(prev, turn):
+        if prev in last_seen:
+            last_seen[prev].insert(0, turn)
+        else:
+            last_seen[prev] = [turn]
+
+    while turn < tgt_num:
+        turn += 1
+        if turn % 100000 == 0:
+            print(turn)
+
+        if len(last_seen[prev]) == 1:
+            prev = 0
+            add_if_exists(prev, turn)
+        else:
+            prev = last_seen[prev][0] - last_seen[prev][1]
+            add_if_exists(prev, turn)
+
+    return prev
+
+
+print(nth_num_said(test_input_1, 2020))  # 436
+print(nth_num_said(test_input_2, 2020))  # 1
+print(nth_num_said(test_input_3, 2020))  # 1259
+print(nth_num_said(input, 2020))  # 1259
+
+print(nth_num_said(test_input_1, 30000000))
