e2cdf7d8016870a45e70504fe6e87577d69ed21b
Jeffrey Marks <jkdmarks@gmail.com>
2020-12-17 21:43:16 -0500

    break if active > 3

--- 2020/day17/solution.py
+++ 2020/day17/solution.py
@@ -25,64 +25,67 @@ test_str = """.#.
 test_input = [list(s) for s in test_str.split("\n")]
 
 
-# def expand_by_one(pos_dict):
-#     expanded = pos_dict.copy()
+def expand_by_one(pos_dict):
+    expanded = pos_dict.copy()
 
-#     for pos in pos_dict:
-#         x, y, z = [int(n) for n in pos.split(",")]
-#         for nbr in neighbors:
-#             check_pos = ",".join([str(a + b) for (a, b) in zip(nbr, (x, y, z))])
-#             if check_pos not in expanded:
-#                 expanded[check_pos] = "."
+    for pos in pos_dict:
+        x, y, z = [int(n) for n in pos.split(",")]
+        for nbr in neighbors:
+            check_pos = ",".join([str(a + b) for (a, b) in zip(nbr, (x, y, z))])
+            if check_pos not in expanded:
+                expanded[check_pos] = "."
 
-#     return expanded
-
-
-# def check_neighbors(pos_dict, pos):
-#     nbr_dict = {".": 0, "#": 0}
-#     x, y, z = [int(n) for n in pos.split(",")]
-#     for nbr in neighbors:
-#         check_pos = ",".join([str(a + b) for (a, b) in zip(nbr, (x, y, z))])
-#         if check_pos not in pos_dict:
-#             nbr_dict["."] += 1
-#         else:
-#             nbr_dict[pos_dict[check_pos]] += 1
-
-#     return nbr_dict
+    return expanded
 
 
-# def run_one_cycle(pos_dict):
-#     pos_dict = expand_by_one(pos_dict)
-#     next = pos_dict.copy()
+def check_neighbors(pos_dict, pos):
+    nbr_dict = {".": 0, "#": 0}
+    x, y, z = [int(n) for n in pos.split(",")]
+    for nbr in neighbors:
+        check_pos = ",".join([str(a + b) for (a, b) in zip(nbr, (x, y, z))])
+        if check_pos not in pos_dict:
+            nbr_dict["."] += 1
+        else:
+            nbr_dict[pos_dict[check_pos]] += 1
 
-#     for (pos, val) in pos_dict.items():
-#         nbrs = check_neighbors(pos_dict, pos)
-#         if val == "#":
-#             if 2 <= nbrs["#"] <= 3:
-#                 next[pos] = "#"
-#             else:
-#                 next[pos] = "."
-#         elif val == ".":
-#             if nbrs["#"] == 3:
-#                 next[pos] = "#"
-#             else:
-#                 next[pos] = "."
+        if nbr_dict["#"] > 3:
+            break
 
-#     return next
+    return nbr_dict
 
 
-# def count_active(input, cycles):
-#     pos_dict = {}
-#     for j in range(len(input)):
-#         for i in range(len(input[j])):
-#             pos = ",".join((str(i), str(j), "0"))
-#             pos_dict[pos] = input[j][i]
+def run_one_cycle(pos_dict):
+    pos_dict = expand_by_one(pos_dict)
+    next = pos_dict.copy()
 
-#     while cycles > 0:
-#         pos_dict = run_one_cycle(pos_dict)
-#         cycles -= 1
+    for (pos, val) in pos_dict.items():
+        nbrs = check_neighbors(pos_dict, pos)
+        if val == "#":
+            if 2 <= nbrs["#"] <= 3:
+                next[pos] = "#"
+            else:
+                next[pos] = "."
+        elif val == ".":
+            if nbrs["#"] == 3:
+                next[pos] = "#"
+            else:
+                next[pos] = "."
 
-#     return list(pos_dict.values()).count("#")
+    return next
+
+
+def count_active(input, cycles):
+    pos_dict = {}
+    for j in range(len(input)):
+        for i in range(len(input[j])):
+            pos = ",".join((str(i), str(j), "0"))
+            pos_dict[pos] = input[j][i]
+
+    while cycles > 0:
+        pos_dict = run_one_cycle(pos_dict)
+        cycles -= 1
+
+    return list(pos_dict.values()).count("#")
 
 
 # print(count_active(test_input, 6))  # 112
@@ -116,6 +119,9 @@ def check_neighbors_4d(pos_dict, pos):
             nbr_dict["."] += 1
         else:
             nbr_dict[pos_dict[check_pos]] += 1
+
+        if nbr_dict["#"] > 3:
+            break
 
     return nbr_dict
 
@@ -154,5 +160,5 @@ def count_active_4d(input, cycles):
     return list(pos_dict.values()).count("#")
 
 
-print(count_active_4d(test_input, 6))  # 848
-print(count_active_4d(input, 6))  # 1812
+# print(count_active_4d(test_input, 6))  # 848
+# print(count_active_4d(input, 6))  # 1812
